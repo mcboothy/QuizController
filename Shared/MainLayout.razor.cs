7b1d41bf3c7f0b793659f5ca6dff4b49fe55619f
Russ <mcboothy@hotmail.co.uk>
2020-12-21 14:59:34 +0000

    signalr test

---
+++ Shared/MainLayout.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using QuizController.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizController.Shared
+{
+    public class MainLayoutComponent : LayoutComponentBase
+    {
+        //[Inject] 
+        //public MainViewModel ViewModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+           //await ViewModel.OnInit();
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace QuizController.ViewModels
+{
+    public class MainViewModel : ComponentBase
+    {
+        private HubConnection hubConnection; //for connecting to SignalR
+        private readonly HttpClient _httpClient = new HttpClient(); //HttpClient for posting messages
+
+        private readonly string functionAppBaseUri = "https://boothy-quiz.azurewebsites.net/api/"; //URL for function app. Leave this as is for now.
+
+        protected override async Task OnInitializedAsync() //actions to do when the page is initialized
+        {
+            try
+            {
+                Messages.Add(new ClientMessage() { Message = "Connecting.." });
+                //create a hub connection to the function app as we'll go via the function for everything SignalR.
+                hubConnection = new HubConnectionBuilder()
+                    .WithUrl(functionAppBaseUri)
+                    .Build();
+
+                Messages.Add(new ClientMessage() { Message = "About to start" });
+
+                //Registers handler that will be invoked when the hub method with the specified method name is invoked.
+                hubConnection.On<ClientMessage>("messages", (clientMessage) =>
+                {
+                    Messages.Add(clientMessage);
+                    StateHasChanged(); //This tells Blazor that the UI needs to be updated
+                });
+
+                await hubConnection.StartAsync(); //start connection!
+            }
+            catch (Exception ex)
+            {
+                Error = $"Failed to setup hub {ex.Message}";
+            }
+        }
+
+        //send our message to the function app
+        public async Task SendAsync()
+        {
+
+            var msg = new ClientMessage
+            {
+                Name = UserInput,
+                Message = MessageInput
+            };
+
+            await _httpClient.PostAsJsonAsync($"{functionAppBaseUri}messages", msg); // post to the function app
+            MessageInput = string.Empty; // clear the message from the textbox
+            StateHasChanged(); //update the UI
+        }
+
+        //Check we're connected
+        public bool IsConnected =>
+            hubConnection.State == HubConnectionState.Connected;
+
+        public List<ClientMessage> Messages { get; private set; }
+            = new List<ClientMessage>(); //List of messages to display
+
+        public HubConnectionState State { get { return hubConnection.State; } }
+
+        public string UserInput { get; set; }
+
+        public string MessageInput { get; set; }
+        public string Error { get; private set; }
+
+        public class ClientMessage
+        {
+            public string Name { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
